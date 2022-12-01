66eb43f161d04f313c5c643eb51e5f9cde4c9eea
Basuk Dmitry <basukdmity@mail.ru>
2022-12-01 18:42:39 +0300

    Добавил задание 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+number = Math.Abs(number);  /// Обозначение модуля
+for (int i = 2; i <= number; i = i + 2)
+{
+  Console.Write($"{i},"); 
+}
